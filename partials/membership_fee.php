507f412af5a89ccf93e02ca5dd477fb4d4a921b9
Neznamy-Samo <46057894+Neznamy-Samo@users.noreply.github.com>
2023-04-25 10:52:52 +0200

    25.4

---
+++ partials/membership_fee.php
@@ -0,0 +1,115 @@
+<div class="table-responsive">
+    <table class="table text-center">
+        <thead>
+        <tr>
+            <th style="width: 34%;">Yearly Access</th>
+
+            <th style="width: 22%;">T1 $420</th>
+
+            <th style="width: 22%;">T2 $640</th>
+
+            <th style="width: 22%;">T3 $860</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <th scope="row" class="text-start">Golf Insurance</th>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+        </tr>
+
+        <tr>
+            <th scope="row" class="text-start">Club Facilities</th>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+        </tr>
+
+        <tr>
+            <th scope="row" class="text-start">Country Club</th>
+
+            <td>
+                <i class="bi-x-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+        </tr>
+
+        <tr>
+            <th scope="row" class="text-start">Weekend Seasonal</th>
+
+            <td>
+                <i class="bi-x-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+        </tr>
+
+        <tr>
+            <th scope="row" class="text-start">Premium Courses</th>
+
+            <td>
+                <i class="bi-x-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-x-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+        </tr>
+
+        <tr>
+            <th scope="row" class="text-start">Pro's Networking</th>
+
+            <td>
+                <i class="bi-x-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-x-circle-fill"></i>
+            </td>
+
+            <td>
+                <i class="bi-check-circle-fill"></i>
+            </td>
+        </tr>
+
+        </tbody>
+    </table>
+</div>
+</div>
