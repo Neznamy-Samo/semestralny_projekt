7f20340b3d26b38d070e397e3e4ac8347e1543a4
Neznamy-Samo <46057894+Neznamy-Samo@users.noreply.github.com>
2023-04-27 10:33:20 +0200

    update_footer

--- index.php
+++ index.php
@@ -94,6 +94,7 @@
                                 <a href="#section_2" class="btn custom-btn smoothscroll me-3">Our Story</a>
 
 
+<<<<<<< Updated upstream
                             </div>
                         </div>
 
@@ -108,6 +109,147 @@
 
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,224L34.3,192C68.6,160,137,96,206,90.7C274.3,85,343,139,411,144C480,149,549,107,617,122.7C685.7,139,754,213,823,240C891.4,267,960,245,1029,224C1097.1,203,1166,181,1234,160C1302.9,139,1371,117,1406,106.7L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
             </section>
+=======
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                    <div class="ratio ratio-16x9">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/MGNgbNGOzh8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,224L34.3,192C68.6,160,137,96,206,90.7C274.3,85,343,139,411,144C480,149,549,107,617,122.7C685.7,139,754,213,823,240C891.4,267,960,245,1029,224C1097.1,203,1166,181,1234,160C1302.9,139,1371,117,1406,106.7L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
+    </section>
+
+
+    <section class="about-section section-padding" id="section_2">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-12 col-12 text-center">
+                    <h2 class="mb-lg-5 mb-4">O našej firme</h2>
+                </div>
+
+                <div class="col-lg-5 col-12 me-auto mb-4 mb-lg-0">
+                    <h3 class="mb-3">História</h3>
+
+
+
+                    <p>Národný golfový klub bol založený 16. mája 1999. 18. júna 1999 bolo slávnostne otvorené zázemie klubu vo forme provizórneho objektu. V prvom roku existencie sa klub v športovej činnosti zameral na výchovu juniorov, ktorí reprezentovali náš  klub už na Juniorských majstrovstvách Slovenska v Košiciach v auguste roku 1999. 31. októbra 1999 bola  prvá sezóna slávnostne ukončená záverečným jesenným turnajom.
+                    </p>
+                </div>
+
+                <?php
+                include_once 'partials/team.php';
+                ?>
+
+            </div>
+        </div>
+    </section>
+
+
+    <section class="section-bg-image">
+        <svg viewBox="0 0 1265 144" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(255, 255, 255, 1)" d="M 0 40 C 164 40 164 20 328 20 L 328 20 L 328 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(255, 255, 255, 1)" d="M 327 20 C 445.5 20 445.5 89 564 89 L 564 89 L 564 0 L 327 0 Z" stroke-width="0"></path> <path fill="rgba(255, 255, 255, 1)" d="M 563 89 C 724.5 89 724.5 48 886 48 L 886 48 L 886 0 L 563 0 Z" stroke-width="0"></path><path fill="rgba(255, 255, 255, 1)" d="M 885 48 C 1006.5 48 1006.5 67 1128 67 L 1128 67 L 1128 0 L 885 0 Z" stroke-width="0"></path><path fill="rgba(255, 255, 255, 1)" d="M 1127 67 C 1196 67 1196 0 1265 0 L 1265 0 L 1265 0 L 1127 0 Z" stroke-width="0"></path></svg>
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 col-12">
+                    <div class="section-bg-image-block">
+                        <h2 class="mb-lg-3">Odoberaj naše novinky</h2>
+
+                        <p>Nenechaj si újsť najnovšie novinky zadaj svoj email s nami budeš vždy napred!</p>
+
+                        <form action="#" method="index.php" class="custom-form mt-lg-4 mt-2" role="form">
+                            <div class="input-group input-group-lg">
+                                <span class="input-group-text bi-envelope" id="basic-addon1"></span>
+
+                                <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Emailová adresa" required="">
+
+                                <button type="submit" class="form-control">Prihlásiť sa</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <svg viewBox="0 0 1265 144" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(255, 255, 255, 1)" d="M 0 40 C 164 40 164 20 328 20 L 328 20 L 328 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(255, 255, 255, 1)" d="M 327 20 C 445.5 20 445.5 89 564 89 L 564 89 L 564 0 L 327 0 Z" stroke-width="0"></path> <path fill="rgba(255, 255, 255, 1)" d="M 563 89 C 724.5 89 724.5 48 886 48 L 886 48 L 886 0 L 563 0 Z" stroke-width="0"></path><path fill="rgba(255, 255, 255, 1)" d="M 885 48 C 1006.5 48 1006.5 67 1128 67 L 1128 67 L 1128 0 L 885 0 Z" stroke-width="0"></path><path fill="rgba(255, 255, 255, 1)" d="M 1127 67 C 1196 67 1196 0 1265 0 L 1265 0 L 1265 0 L 1127 0 Z" stroke-width="0"></path></svg>
+    </section>
+
+
+    <section class="membership-section section-padding" id="section_3">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-12 col-12 text-center mx-auto mb-lg-5 mb-4">
+                    <h2><span>Predplatné</span> do národného golfového klubu</h2>
+                </div>
+
+                <div class="col-lg-6 col-12 mb-3 mb-lg-0">
+                    <h4 class="mb-4 pb-lg-2">Predplatné</h4>
+
+                    <?php
+                    include_once 'partials/membership_fee.php';
+                    ?>
+
+                    <div class="col-lg-5 col-12 mx-auto">
+                        <h4 class="mb-4 pb-lg-2">Pridaj sa k nám!</h4>
+                        <form action="#" method="index.php" class="custom-form membership-form shadow-lg" role="form">
+                            <h4 class="text-white mb-4">Staň sa členom</h4>
+
+                            <div class="form-floating">
+                                <input type="text" name="full-name" id="full-name" class="form-control" placeholder="Full Name" required="">
+
+                                <label for="floatingInput">Meno priezvisko</label>
+                            </div>
+
+                            <div class="form-floating">
+                                <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Email address" required="">
+
+                                <label for="floatingInput">Emailová adresa</label>
+                            </div>
+
+                            <div class="form-floating">
+                                <textarea class="form-control" id="message" name="message" placeholder="Describe message here"></textarea>
+
+                                <label for="floatingTextarea"> Správa</label>
+                            </div>
+
+                            <button type="submit" class="form-control">Odoslať</button>
+                    </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <section class="events-section section-bg section-padding" id="section_4">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-12 col-12">
+                    <h2 class="mb-lg-3">Nastávajúce udalosti</h2>
+                </div>
+
+                <div class="row custom-block mb-3">
+                    <?php
+                    include_once 'partials/events.php';
+                    ?>
+    </section>
+</main>
+
+<?php
+include_once 'partials/footer.php';
+?>
+>>>>>>> Stashed changes
 
 
             <section class="about-section section-padding" id="section_2">
--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,74 @@
 <footer class="site-footer">
+    <section class="contact-section section-padding" id="section_5">
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-5 col-12">
+                    <form action="#" method="post" class="custom-form contact-form" role="form">
+                        <h2 class="mb-4 pb-2">Kontaktujte nás</h2>
+
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="form-floating">
+                                    <input type="text" name="full-name" id="full-name" class="form-control" placeholder="Full Name" required="">
+
+                                    <label for="floatingInput">Meno a priezvisko</label>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="form-floating">
+                                    <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Email address" required="">
+
+                                    <label for="floatingInput">Emailová addresa</label>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12 col-12">
+                                <div class="form-floating">
+                                    <textarea class="form-control" id="message" name="message" placeholder="Describe message here"></textarea>
+
+                                    <label for="floatingTextarea">Správa</label>
+                                </div>
+
+                                <button type="submit" class="form-control">Odoslať formulár</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                    <div class="contact-info mt-5">
+                        <div class="contact-info-item">
+                            <div class="contact-info-body">
+                                <strong>Bratislava, Slovensko</strong>
+
+                                <p class="mt-2 mb-1">
+                                    <a href="tel: 010-020-0340" class="contact-link">
+                                        (+421)
+                                        423-320-0340
+                                    </a>
+                                </p>
+
+                                <p class="mb-0">
+                                    <a href="mailto:info@company.com" class="contact-link">
+                                        narodnyklub@info.com
+                                    </a>
+                                </p>
+                            </div>
+
+
+                        </div>
+
+                        <img src="images/WorldMap.svg" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
     <div class="container">
         <div class="row">
 
